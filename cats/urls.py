3865c14ae8a7d179875464be4df01858e9aa0b77
fdsfds-web <martynenkovlad97@gmail.com>
2025-04-27 17:16:16 +0300

    Some models added

---
+++ cats/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+
+from django.urls import include, path
+
+from cats.views import CatViewSet, OwnerViewSet
+
+
+router = DefaultRouter()
+router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
